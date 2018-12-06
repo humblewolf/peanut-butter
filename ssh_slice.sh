b0be33272dc8eb7b987fb47a4249f5ad2e9fdf94
humble_wolf <35303917+humblewolf@users.noreply.github.com>
2018-12-07 01:05:43 +0530

    Update ssh_slice.sh

--- ssh_slice.sh
+++ ssh_slice.sh
@@ -1,5 +1,5 @@
 #to generate a new pub-pri pair of ssh keys
 ssh-keygen -t rsa
 
-#to copy public key to target machine
+#to copy public key to target machine, passworded ssh auth shall be enabled before this and should be disable after this. 
 ssh-copy-id -i ~/.ssh/id_rsa vagrant@192.168.33.10
